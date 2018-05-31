631477b864e6e179163f77161d35d8d0fec4ede9
zeshunzong <37764481+zeshunzong@users.noreply.github.com>
2018-05-31 18:07:11 +0800

    Update 198.py

--- 198.py
+++ 198.py
@@ -1,14 +1,24 @@
-def rob(arr, i, dictionary):
-    if i==0:
-        money = arr[0]
-    elif i==1:
-        money = max(arr[0:2])
-    elif i in dictionary:
-        money = dictionary[i]
-    else:
-        money = max([rob(arr, i-2, dictionary)+arr[i], rob(arr, i-1, dictionary)])
-        dictionary[i] = money
-    return money
-arr = [1,2,3,1]
-d1 = {}
-print(rob(arr, len(arr)-1, d1))
+class Solution:
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d1 = {}
+        return self.rob_dp(nums, len(nums)-1, d1)
+        
+    def rob_dp(self, arr, i, dictionary):
+        if i<0:
+            return 0
+        if i==0:
+            money = arr[0]
+        elif i==1:
+            money = max(arr[0:2])
+        elif i in dictionary:
+            money = dictionary[i]
+        else:
+            money = max([self.rob_dp(arr, i-2, dictionary)+arr[i], self.rob_dp(arr, i-1, dictionary)])
+            dictionary[i] = money
+        return money
+        
+        
